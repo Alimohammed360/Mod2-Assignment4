e7eb52c9dfdbd2cc381accbbd27b7c69d399d666
Alimohammed <alimohamedwadiwala@gmail.comgit config --global user.email alimohamedwadiwala@gmail.com>
2023-12-30 21:11:52 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+import "./App.css"
+import {OceanImg, Head,BootstrapReady,MainOver,MainOver2, MainOver3, People, Ocean2} from "./component/firstComp"
+const App = ()=>{
+  return <div >
+    <Head/><OceanImg/><BootstrapReady/><MainOver/><MainOver2/><MainOver3/><People/><Ocean2/>
+  </div>
+}
+export default App
